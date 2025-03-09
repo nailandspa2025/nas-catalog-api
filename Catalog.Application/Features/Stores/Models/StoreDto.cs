b6195d3d66ee95917e024135d1effdc80db35cb5
“canhvanlam” <canhvanlam@salescore.me>
2025-03-09 15:24:00 +0700

    store

--- Catalog.Application/Features/Stores/Models/StoreDto.cs
+++ Catalog.Application/Features/Stores/Models/StoreDto.cs
@@ -1,11 +1,10 @@
-﻿using System;
-using AutoMapper;
+﻿using AutoMapper;
 using BuildingBlocks.Persistence.Models;
 using Catalog.Domain.Entities;
 
 namespace Catalog.Application.Features.Stores.Models
 {
-	public class StoreDto: BaseAuditableDto
+    public class StoreDto: BaseAuditableDto
     {
         public long Id { get; set; }
 
@@ -23,36 +22,25 @@ namespace Catalog.Application.Features.Stores.Models
 
         public string? Hotline { get; set; }
 
-<<<<<<< HEAD
-        public DateTime? OperatingHours { get; set; }
-=======
         public TimeSpan OpenTime { get; set; }
 
         public TimeSpan CloseTime { get; set; }
->>>>>>> 3399663 (product)
 
         public string? GoogleReviewLink { get; set; }
 
         public string OwnerId { get; set; } = null!;
 
-<<<<<<< HEAD
-=======
         public List<long> ProductIds { get; set; } = new List<long>();
 
         public List<string> ProductsNames { get; set; } = new List<string>();
 
->>>>>>> 3399663 (product)
         private class Mapping : Profile
         {
             public Mapping()
             {
-<<<<<<< HEAD
-                CreateMap<Store, StoreDto>();
-=======
                 CreateMap<Store, StoreDto>()
                    .ForMember(dest => dest.ProductIds, opt => opt.MapFrom(src => src.Products.Select(p => p.Id).ToList()))
                    .ForMember(dest => dest.ProductsNames, opt => opt.MapFrom(src => src.Products.Select(p => p.ProductName).ToList()));
->>>>>>> 3399663 (product)
             }
         }
     }
