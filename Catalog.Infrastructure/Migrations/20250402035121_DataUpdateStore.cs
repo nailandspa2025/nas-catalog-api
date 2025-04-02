89ac7b35ad83cac32331bfbe0e1e2e6e634868c6
“canhvanlam” <canhvanlam@salescore.me>
2025-04-02 11:25:56 +0700

    update favorite

---
+++ Catalog.Infrastructure/Migrations/20250402035121_DataUpdateStore.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Catalog.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class DataUpdateStore : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsFavorite",
+                table: "Store",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsFavorite",
+                table: "Store");
+        }
+    }
+}
