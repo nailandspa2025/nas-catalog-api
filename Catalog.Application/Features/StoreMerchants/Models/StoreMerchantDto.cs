8d0c93f298a2db370f97a66cc07a645e6640c3a4
“canhvanlam” <canhvanlam@salescore.me>
2025-04-16 16:54:07 +0700

    store by merchant

---
+++ Catalog.Application/Features/StoreMerchants/Models/StoreMerchantDto.cs
@@ -0,0 +1,55 @@
+﻿using AutoMapper;
+using BuildingBlocks.Persistence.Models;
+using Catalog.Domain.Entities;
+
+namespace Catalog.Application.Features.StoreMerchants.Models;
+
+public class StoreMerchantDto: BaseAuditableDto
+{
+    public long Id { get; set; }
+
+    public string StoreName { get; set; } = null!;
+
+    public string? Avatar { get; set; }
+
+    public string? AddressStore { get; set; }
+
+    public int RatingStar { get; set; }
+
+    public double Lat { get; set; }
+
+    public double Lng { get; set; }
+
+    public string? Hotline { get; set; }
+
+    public TimeSpan OpenTime { get; set; }
+
+    public TimeSpan CloseTime { get; set; }
+
+    public string? GoogleReviewLink { get; set; }
+
+    public string OwnerId { get; set; } = null!;
+
+    public List<long> ProductIds { get; set; } = new List<long>();
+
+    public List<string> ProductsNames { get; set; } = new List<string>();
+
+    public List<string> ImageUrls { get; set; } = new List<string>();
+
+    public List<string> UserIds { get; set; } = new List<string>();
+
+    public bool IsFavorite { get; set; }
+
+    private class Mapping : Profile
+    {
+        public Mapping()
+        {
+            CreateMap<Store, StoreMerchantDto>()
+               .ForMember(dest => dest.ProductIds, opt => opt.MapFrom(src => src.Products.Select(p => p.Id).ToList()))
+               .ForMember(dest => dest.ProductsNames, opt => opt.MapFrom(src => src.Products.Select(p => p.ProductName).ToList()))
+               .ForMember(dest => dest.ImageUrls, opt => opt.MapFrom(src => src.ImageGallerys.Select(i => i.Url).ToList()))
+               .ForMember(dest => dest.UserIds, opt => opt.MapFrom(src => src.UserStores.Select(i => i.UserId).ToList()));
+        }
+    }
+}
+
---
+++ Catalog.Application/Features/StoreMerchants/Queries/GetStoreMerchantsWithPagination/GetStoreMerchantsWithPaginationQuery.cs
@@ -0,0 +1,58 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using BuildingBlocks.Authentication.Abstractions;
+using BuildingBlocks.Common.Extensions;
+using BuildingBlocks.Common.Mappings;
+using BuildingBlocks.Core.Response;
+using Catalog.Application.Common.Interfaces;
+using Catalog.Application.Features.StoreMerchants.Models;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Catalog.Application.Features.StoreMerchants.Queries.GetStoreMerchantsWithPagination;
+
+public record GetStoreMerchantsWithPaginationQuery: IRequest<ApiResponse<PaginatedList<StoreMerchantDto>>>
+{
+    public int PageNumber { get; set; } = 1;
+
+    public int PageSize { get; set; } = 10;
+
+    public string? SearchText { get; set; }
+}
+
+public class GetStoreMerchantsWithPaginationQueryHandler : IRequestHandler<GetStoreMerchantsWithPaginationQuery, ApiResponse<PaginatedList<StoreMerchantDto>>>
+{
+    private readonly ICatalogDbContext _context;
+    private readonly IMapper _mapper;
+    private readonly ICurrentUser _currentUser;
+
+    public GetStoreMerchantsWithPaginationQueryHandler(ICatalogDbContext context, IMapper mapper, ICurrentUser currentUser)
+    {
+        _context = context;
+        _mapper = mapper;
+        _currentUser = currentUser;
+    }
+
+    public async Task<ApiResponse<PaginatedList<StoreMerchantDto>>> Handle(GetStoreMerchantsWithPaginationQuery request, CancellationToken cancellationToken)
+    {
+        var paramSearchText = (request.SearchText ?? string.Empty).ToUpper();
+        var query = _context.Store
+            .Where(s => !s.IsDeleted &&
+                        _context.UserStore.Any(us => us.UserId == _currentUser.UserId && us.StoreId == s.Id))
+            .AsNoTracking();
+
+        if (!paramSearchText.IsNullOrEmpty())
+        {
+            query = query.Where(s => paramSearchText.Contains(s.StoreName));
+        }
+
+        var paginationResult = await query
+            
+            .OrderBy(x => x.Created)
+            .ProjectTo<StoreMerchantDto>(_mapper.ConfigurationProvider)
+            .PaginatedListAsync(request.PageNumber, request.PageSize);
+
+        return ApiResponse<PaginatedList<StoreMerchantDto>>.Success(paginationResult);
+
+    }
+}
