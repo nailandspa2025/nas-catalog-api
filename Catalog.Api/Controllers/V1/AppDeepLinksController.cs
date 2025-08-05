c961376dd1dfcae3edaf00603d6ebe298d280909
canhvanlam <canhvanlam@gmail.com>
2025-08-05 23:17:16 +0700

    fix ios

--- Catalog.Api/Controllers/V1/AppDeepLinksController.cs
+++ Catalog.Api/Controllers/V1/AppDeepLinksController.cs
@@ -1,4 +1,4 @@
-using BuildingBlocks.Core.Response;
+﻿using BuildingBlocks.Core.Response;
 using Catalog.Application.Features.AppDeepLinks.Commands.CreateAppDeepLink;
 using Catalog.Application.Features.AppDeepLinks.Models;
 using Catalog.Application.Features.AppDeepLinks.Queries.GetAppDeepLink;
@@ -27,17 +27,44 @@ public class AppDeepLinksController : ApiControllerBase
     [AllowAnonymous]
     [HttpGet("{code}")]
     [ProducesResponseType(typeof(ApiResponse<AppDeepLinkDto>), StatusCodes.Status200OK)]
-    public async Task<ActionResult<ApiResponse<AppDeepLinkDto>>> GetByCodeAsync(string code)
+    public async Task<IActionResult> GetByCodeAsync(string code)
     {
         var result = await Mediator.Send(new GetAppDeepLinkByCodeQuery { Code = code });
-        if(!result.Succeeded) return NotFound(result);
+        if (!result.Succeeded)
+            return NotFound(result);
+
         var dto = result.Data;
         var userAgent = Request.Headers["User-Agent"].ToString().ToLower();
-        var redirectUrl = dto.WebFallback;
-        if (userAgent.Contains("iphone") || userAgent.Contains("ipad") || userAgent.Contains("ios"))
-            redirectUrl = dto.IOSLink;
-        else if (userAgent.Contains("android"))
+
+        string redirectUrl = dto.WebFallback;
+
+        if (userAgent.Contains("android"))
+        {
             redirectUrl = dto.AndroidLink;
+        }
+        else if (userAgent.Contains("iphone") || userAgent.Contains("ipad") || userAgent.Contains("ios"))
+        {
+            return Content($@"
+            <!DOCTYPE html>
+            <html>
+            <head>
+                <meta charset='UTF-8'>
+                <title>Redirecting...</title>
+                <script type='text/javascript'>
+                    window.onload = function() {{
+                        window.location = '{dto.IOSLink}';
+                        setTimeout(function() {{
+                            window.location = 'https://apps.apple.com/us/app/nas-nail-spa/id6746377567';
+                        }}, 2000);
+                    }};
+                </script>
+            </head>
+            <body>
+                <p>Đang chuyển hướng...</p>
+            </body>
+            </html>
+        ", "text/html");
+        }
         return Redirect(redirectUrl);
     }
 }
