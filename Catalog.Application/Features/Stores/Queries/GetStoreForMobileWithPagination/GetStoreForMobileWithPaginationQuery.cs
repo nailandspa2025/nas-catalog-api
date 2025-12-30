be14c37f32216a02b897c501494fadf3639956d4
canhvanlam <canhvanlam@gmail.com>
2025-12-30 15:36:55 +0700

    tìm cưa hàng gần nhất

--- Catalog.Application/Features/Stores/Queries/GetStoreForMobileWithPagination/GetStoreForMobileWithPaginationQuery.cs
+++ Catalog.Application/Features/Stores/Queries/GetStoreForMobileWithPagination/GetStoreForMobileWithPaginationQuery.cs
@@ -10,7 +10,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Catalog.Application.Features.Stores.Queries.GetStoreForMobileWithPagination;
 
-public record GetStoreForMobileWithPaginationQuery: IRequest<ApiResponse<PaginatedList<StoreDto>>>
+public record GetStoreForMobileWithPaginationQuery : IRequest<ApiResponse<PaginatedList<StoreDto>>>
 {
     public int PageNumber { get; init; } = 1;
 
@@ -19,6 +19,9 @@ public record GetStoreForMobileWithPaginationQuery: IRequest<ApiResponse<Paginat
     public string? SearchText { get; init; }
 
     public int? Rating { get; init; }
+    public double? Lat { get; init; }
+    public double? Long { get; init; }
+
 }
 
 public class GetStoreForMobileWithPaginationQueryHandler : IRequestHandler<GetStoreForMobileWithPaginationQuery, ApiResponse<PaginatedList<StoreDto>>>
@@ -26,10 +29,10 @@ public class GetStoreForMobileWithPaginationQueryHandler : IRequestHandler<GetSt
     private readonly ICatalogDbContext _context;
     private readonly IMapper _mapper;
 
-    public GetStoreForMobileWithPaginationQueryHandler (ICatalogDbContext context, IMapper mapper)
+    public GetStoreForMobileWithPaginationQueryHandler(ICatalogDbContext context, IMapper mapper)
     {
         _context = context;
-        _mapper = mapper;  
+        _mapper = mapper;
     }
     public async Task<ApiResponse<PaginatedList<StoreDto>>> Handle(GetStoreForMobileWithPaginationQuery request, CancellationToken cancellationToken)
     {
@@ -47,12 +50,30 @@ public class GetStoreForMobileWithPaginationQueryHandler : IRequestHandler<GetSt
         {
             query = query.Where(s => s.RatingStar == request.Rating.Value);
         }
+        if (request.Lat.HasValue && request.Long.HasValue)
+        {
+            var lat = request.Lat.Value;
+            var lon = request.Long.Value;
+            query = query.OrderBy(s =>
+                6371 * Math.Acos(
+                    Math.Cos(lat * Math.PI / 180) *
+                    Math.Cos(s.Lat * Math.PI / 180) *
+                    Math.Cos((s.Lng - lon) * Math.PI / 180) +
+                    Math.Sin(lat * Math.PI / 180) *
+                    Math.Sin(s.Lat * Math.PI / 180)
+                )
+            );
+        }
+        else
+        {
+            query = query.OrderBy(x => x.Order).ThenByDescending(x => x.Created);
+        }
         var paginationResult = await query
-            .OrderBy(x => x.Order)
-            .ThenByDescending(x => x.Created)
             .ProjectTo<StoreDto>(_mapper.ConfigurationProvider)
             .PaginatedListAsync(request.PageNumber, request.PageSize);
 
         return ApiResponse<PaginatedList<StoreDto>>.Success(paginationResult);
     }
+    private static double ToRadians(double angle)
+    => Math.PI * angle / 180.0;
 }
