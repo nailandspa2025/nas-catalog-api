536cd1e8c4f1a06caa53cb1efd45558abaa6d7e8
canhvanlam <canhvanlam@gmail.com>
2026-06-09 15:30:21 +0700

    fix code

--- Catalog.Domain/Entities/PaymentProviderSetting.cs
+++ Catalog.Domain/Entities/PaymentProviderSetting.cs
@@ -7,6 +7,5 @@ public class PaymentProviderSetting : BaseAuditableEntity<int>
     public int PaymentProviderId { get; set; }
     public PaymentProvider PaymentProvider { get; set; } = null!;
     public string Key { get; set; } = string.Empty;
-
-    public string Value { get; set; } = string.Empty;
+    public string ? Value { get; set; }
 }
